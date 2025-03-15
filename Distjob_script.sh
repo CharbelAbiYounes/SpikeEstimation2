925d636b5ee272076034fc8c400a8db5e59fa12d
CharbelAbiYounes <cyounes@uw.edu>
2025-03-15 00:45:01 -0700

    .

--- Distjob_script.sh
+++ Distjob_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=cpu-g2     # Partition to use
 #SBATCH --nodes=1               # Request 1 node
 #SBATCH --cpus-per-task=90      # Request CPUs
-#SBATCH --mem=1200G              # Memory allocation
+#SBATCH --mem=900G              # Memory allocation
 #SBATCH --time=72:00:00         # Maximum runtime
 #SBATCH --output=cyounes-job-%j.out  # Standard output log
 #SBATCH --error=cyounes-job-%j.err   # Standard error log
